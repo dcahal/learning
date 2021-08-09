9e57b9536835cdb1dbb40e15e6eee409e9c4c3c5
David <86898130+narodn1k@users.noreply.github.com>
2021-08-09 00:49:25 -0400

    Comment py4e 6.1

--- exercises/py4e/6e1.py
+++ exercises/py4e/6e1.py
@@ -1,10 +1,13 @@
 # Exercise 1: Write a while loop that starts at the last character in the string and works its way backwards to the first character in the string, printing each letter on a separate line, except backwards.
 
 spaceship = 'galactica'
-print(len(spaceship))
+ifirst = 0 - len(spaceship)
+print(ifirst)
+
+# Index 0 starts second iteration of string. Start with index -1, the last letter.
 print("Starting:")
 i = -1
-while i >= 0-len(spaceship):
+while i >= ifirst:
     letter = spaceship[i]
     print(letter)
     i = i - 1
