976069f17dbecd224800b28153d684d3e3b47d6a
David <86898130+narodn1k@users.noreply.github.com>
2021-08-18 23:46:41 -0400

    pyfe: Strip 7e1 whitespace to match example

--- pyfe/exercises/7e1.py
+++ pyfe/exercises/7e1.py
@@ -1,5 +1,8 @@
+# Exercise 1: Write a program to read through a file and print the contents of the file (line by line) all in upper case. Executing the program will look as follows:
+
 mbox = open('mbox-short.txt')
 num = 0
 for line in mbox:
-    print(line.upper())
+    line = line.upper()
+    print(line.rstrip())
     num = num + 1
