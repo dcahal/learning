a43b2cac1c00fa86a14709716268178647ce80a7
David <86898130+narodn1k@users.noreply.github.com>
2021-08-19 23:51:14 -0400

    pyfe: 7.1 works on example file

--- pyfe/exercises/7e1.py
+++ pyfe/exercises/7e1.py
@@ -1,6 +1,6 @@
 # Exercise 1: Write a program to read through a file and print the contents of the file (line by line) all in upper case. Executing the program will look as follows:
 
-mbox = open('../notes/mbox-short.txt')
+mbox = open('../examples/mbox-short.txt')
 num = 0
 for line in mbox:
     line = line.upper()
