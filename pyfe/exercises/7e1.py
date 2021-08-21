25df92c3d375f3d0d2e3cac362ed35897202b001
David <86898130+narodn1k@users.noreply.github.com>
2021-08-20 23:34:51 -0400

    pyfe: Book files now in 'provided'

--- pyfe/exercises/7e1.py
+++ pyfe/exercises/7e1.py
@@ -1,6 +1,6 @@
 # Exercise 1: Write a program to read through a file and print the contents of the file (line by line) all in upper case. Executing the program will look as follows:
 
-mbox = open('../examples/mbox-short.txt')
+mbox = open('../provided/mbox-short.txt')
 num = 0
 for line in mbox:
     line = line.upper()
---
+++ pyfe/provided/README.md
@@ -0,0 +1,1 @@
+### Files provided by the book or lectures for use in exercises, as solutions, etc.
