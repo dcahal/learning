62de50a77fecc48f4b8ef4f826c7cd9e9964ce8e
David <86898130+narodn1k@users.noreply.github.com>
2021-08-27 23:17:43 -0400

    pyfe: Add explainer for 7.2

--- pyfe/exercises/7e2.py
+++ pyfe/exercises/7e2.py
@@ -1,3 +1,9 @@
+# Exercise 2: Write a program to prompt for a file name, and then read through the file and look for lines of the form:
+
+# When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidencevalues from these lines. When you reach the end of the file, print out the average spam confidence.
+
+
+
 mbox = open('../provided/mbox-short.txt')
 
 count = 0
