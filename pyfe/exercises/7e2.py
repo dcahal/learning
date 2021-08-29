a0e2a3e1f711eb8e4d073a1562464a7d0dd4c8c4
David <86898130+narodn1k@users.noreply.github.com>
2021-08-29 00:54:59 -0400

    Complete pyfe 7.2

--- pyfe/exercises/7e2.py
+++ pyfe/exercises/7e2.py
@@ -3,41 +3,24 @@
 # When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidencevalues from these lines. When you reach the end of the file, print out the average spam confidence.
 
 
-# sample = input("File to parse: ")
-# handle = open(sample)
-
+sample = input("File to parse: ")
+opened = open(sample)
 
 count = 0
 total = 0
-value = 0
 
-handle = open("../provided/mbox-short.txt")
-
-
-def find_totals(opened) :
-    for value in opened:
+for line in opened:
+    # May use line.startswith() for this problem. This solution supports inline matches
+    found = line.find('X-DSPAM-Confidence:')
+    if found == -1 :
+        continue
+    else :
         count = count + 1
+        # Found match at rune #, grab text after 19 character pattern
+        grab = line[found+19:].strip()
+        value  = float(grab)
         total = total + value
         mean = total / count
 
-
-def find_dspam(opened) :
-    count = 0
-    total = 0
-    for line in opened:
-        found = line.find('X-DSPAM-Confidence:')
-        if found == -1 :
-            continue
-        grab = line[found+19:]
-        num = grab.strip()
-        value  = float(num)
-        count = count + 1
-        total = total + value
-        mean = total / count
-        print(count)
-
-find_dspam(handle)
-
-#mean = total / count
-#print("Matching lines: " , count)    
-print("Average confidence value: " , mean)
+print("Matching lines:" , count)    
+print("Average confidence value:" , mean)
