5f26b51aa851c02e71c6ecd1535b48d110ba2b9f
David <86898130+narodn1k@users.noreply.github.com>
2021-08-28 23:58:20 -0400

    pyfe: 7.2 drop second function

--- pyfe/exercises/7e2.py
+++ pyfe/exercises/7e2.py
@@ -3,26 +3,41 @@
 # When you encounter a line that starts with “X-DSPAM-Confidence:” pull apart the line to extract the floating-point number on the line. Count these lines and then compute the total of the spam confidencevalues from these lines. When you reach the end of the file, print out the average spam confidence.
 
 
-sample = input("File to parse: ")
-handle = open(sample)
+# sample = input("File to parse: ")
+# handle = open(sample)
 
 
 count = 0
-sum = 0
+total = 0
 value = 0
 
-def find_dspam():
-    for line in handle:
-        found = line.find('X-DSPAM-Confidence:')
-        grab = line[found+1:]
-        # num = grab.lstrip()
-        # Python trims white space during float conversion
-        value  = float(grab)
+handle = open("../provided/mbox-short.txt")
 
-def find_totals():
-    for value in handle:
+
+def find_totals(opened) :
+    for value in opened:
         count = count + 1
-        sum = sum + value
-            
+        total = total + value
+        mean = total / count
 
 
+def find_dspam(opened) :
+    count = 0
+    total = 0
+    for line in opened:
+        found = line.find('X-DSPAM-Confidence:')
+        if found == -1 :
+            continue
+        grab = line[found+19:]
+        num = grab.strip()
+        value  = float(num)
+        count = count + 1
+        total = total + value
+        mean = total / count
+        print(count)
+
+find_dspam(handle)
+
+#mean = total / count
+#print("Matching lines: " , count)    
+print("Average confidence value: " , mean)
