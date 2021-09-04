b9f042c57bfee7771dc4e3729215577beb13a75d
David <86898130+narodn1k@users.noreply.github.com>
2021-09-03 23:48:25 -0400

    pyfe: start 7.3

---
+++ pyfe/exercises/7e3.py
@@ -0,0 +1,13 @@
+# Exercise 3: Modify the program that prompts the user for the file name so that it prints a funny message when the user types in the exact file name “na na boo boo”. The program should behave normally for all other files which exist and don’t exist.
+
+fname = input('Enter the file name: ')
+try:
+    fhand = open(fname)
+except:
+    print('File cannot be opened:', fname)
+    exit()
+count = 0
+for line in fhand:
+    if line.startswith('Subject:'):
+        count = count + 1
+print('There were', count, 'subject lines in', fname)
