68d559febb34a316bc56dca04db063cb4178a447
David <86898130+narodn1k@users.noreply.github.com>
2021-09-07 23:43:54 -0400

    Complete pyfe 8.1

--- pyfe/exercises/8e1.py
+++ pyfe/exercises/8e1.py
@@ -1,11 +1,15 @@
 # Exercise 1: Write a function called chop that takes a list and modifies it, removing the first and last elements, and returns None. Then write a function called middle that takes a list and returns a new list that contains all but the first and last elements.
 
-items = []
+items = ['a', 'b', 'c', 'd']
 
 def chop(ls) :
     ls.pop(0)
-    ls.pop
-    return None
+    ls.pop(-1)
 
 def middle(ls) :
-    
+    del ls[0]
+    del ls[-1]
+
+print(chop(items))
+print(middle(items))
+print(items)
